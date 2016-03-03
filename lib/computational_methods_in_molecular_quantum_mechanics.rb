9236e204a555125c692bbc808609d4e80c9d9d11
Joshua Cook <joshua.cook@telescope.tv>
2016-03-03 15:31:38 -0800

    init

---
+++ lib/computational_methods_in_molecular_quantum_mechanics.rb
@@ -0,0 +1,5 @@
+require "computational_methods_in_molecular_quantum_mechanics/version"
+
+module ComputationalMethodsInMolecularQuantumMechanics
+  # Your code goes here...
+end
---
+++ lib/computational_methods_in_molecular_quantum_mechanics/version.rb
@@ -0,0 +1,3 @@
+module ComputationalMethodsInMolecularQuantumMechanics
+  VERSION = "0.1.0"
+end
